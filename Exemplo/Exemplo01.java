150f91888dc5fe99b393ea35fc6982febfef7684
LucasDeSousa <lucassousa1710@hotmail.com>
2021-03-24 15:17:55 -0300

    aula 2 vetore

---
+++ Exemplo/Exemplo01.java
@@ -0,0 +1,19 @@
+package Exemplo;
+
+
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        int[] numeros = new int[11];
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (i + 1);
+
+        }
+
+        for (int i = 0; i < numeros.length ; i++) {
+            System.out.println(numeros[i]);
+
+        }
+    }
+}
